a52630ebc107a3744a54ceedfadc59bea22205e6
VigneshSK17 <vigsk17@gmail.com>
2024-07-16 19:35:00 -0400

    Added tests

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,12 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
-
+if os.getenv('TESTING') == 'true':
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+    
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
