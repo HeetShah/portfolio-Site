335eb9ab8fefeece14048ebcec2ba906b8df62ad
HeetShah <heetshah7642@gmail.com>
2024-07-26 00:49:42 -0400

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# This script is used to redeploy the Flask application using systemd. Here is what it does:
+# This script is used to redeploy the Flask application using Docker.
+# Here is what it does:
 # 1. Navigate to the project directory.
 # 2. Fetch the latest changes from the main branch of the GitHub repository.
 # 3. Reset the local repository to match the main branch.
-# 4. Install the required Python dependencies.
-# 5. Restart the 'myportfolio' service to apply the changes.
+# 4. Take down the existing Docker containers to prevent out of memory issues.
+# 5. Rebuild and start the Docker containers in detached mode.
 
 PROJECT_DIR="/root/portfolio-Site"
-VENV_PATH="$PROJECT_DIR/python3-virtualenv"
 
 # Navigate to the project directory
 cd "$PROJECT_DIR"
@@ -15,9 +15,8 @@ cd "$PROJECT_DIR"
 # Fetch and reset to the latest changes from GitHub
 git fetch && git reset --hard origin/main
 
-# Activate virtual environment and install dependencies
-source "$VENV_PATH/bin/activate"
-pip install -r requirements.txt
+# Take down existing Docker containers
+docker compose -f docker-compose.prod.yml down
 
-# Restart the 'myportfolio' service to apply new changes
-sudo systemctl restart myportfolio.service
+# Rebuild and start Docker containers in detached mode
+docker compose -f docker-compose.prod.yml up -d --build
